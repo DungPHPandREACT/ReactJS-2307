16d31ec4e0f0c96feb59d9ba2f990370fa5ebad4
DUNGPHPANDREACT <dung2000cn@gmail.com>
2023-09-25 18:42:38 +0700

    add student

--- Lesson12/Manage_Student/index.js
+++ Lesson12/Manage_Student/index.js
@@ -29,17 +29,18 @@ const listStudent = [
 	},
 ];
 
-// In thông tin học sinh ra
-let stringHTML = '';
+function renderData(data = []) {
+	// In thông tin học sinh ra
+	let stringHTML = '';
 
-for (let i = 0; i < listStudent.length; i++) {
-	stringHTML += `
+	for (let i = 0; i < data.length; i++) {
+		stringHTML += `
     <tr>
-		<td scope="col">${listStudent[i].id}</td>
-		<td scope="col">${listStudent[i].name}</td>
-		<td scope="col">${listStudent[i].age}</td>
-		<td scope="col">${listStudent[i].gender}</td>
-		<td scope="col">${listStudent[i].point}</td>
+		<td scope="col">${data[i].id}</td>
+		<td scope="col">${data[i].name}</td>
+		<td scope="col">${data[i].age}</td>
+		<td scope="col">${data[i].gender}</td>
+		<td scope="col">${data[i].point}</td>
 		<td scope="col">
             <div>
                 <button class="btn btn-success" style="margin: 0 4px">
@@ -52,23 +53,52 @@ for (let i = 0; i < listStudent.length; i++) {
 		</td>
 	</tr>
     `;
+	}
+
+	// console.log(stringHTML);
+	document.getElementById('data-student').innerHTML = stringHTML;
 }
 
-// console.log(stringHTML);
-document.getElementById('data-student').innerHTML = stringHTML;
+// function clear input
+function clearInput() {
+	document.getElementById('id').value = '';
+	document.getElementById('name').value = '';
+	document.getElementById('age').value = '';
+	document.getElementById('gender').value = '';
+	document.getElementById('point').value = '';
+}
 
 // Add student
 // Bước 1: Gán sự kiện on click cho button "Add student"
-// Bước 2: Trong hàm xử lý sự kiện
-    // -Lấy được dữ liệu người dùng đang nhập ở input
-    // -Tạo 1 object mới chứa các thông tin vừa lấy được, push vào mảng
-    // -
+document.getElementById('btn-add').onclick = function () {
+	// Bước 2: Trong hàm xử lý sự kiện
+	// -Lấy được dữ liệu người dùng đang nhập ở input
+	const id = document.getElementById('id').value;
+	const name = document.getElementById('name').value;
+	const age = document.getElementById('age').value;
+	const gender = document.getElementById('gender').value;
+	const point = document.getElementById('point').value;
+	// -Tạo 1 object mới chứa các thông tin vừa lấy được, push vào mảng
+	const newStudent = {
+		id: id,
+		name: name,
+		age: age,
+		gender: gender,
+		point: point,
+	};
+	listStudent.push(newStudent);
+	clearInput();
+	renderData(listStudent);
+	// -
+};
 
 // Delete student
 // Bước 1: Gán sự kiện onclick cho các button delete
 // Bước 2: Trong hàm xử lý sự kiện
-    // -Lấy được id của học sinh đang được click
-    // -Tìm vị trí index của học sinh đó trong listStudent
-    // -Xóa học sinh đó trong mảng listStudent
+// -Lấy được id của học sinh đang được click
+// -Tìm vị trí index của học sinh đó trong listStudent
+// -Xóa học sinh đó trong mảng listStudent
 
 // Khi reload lại trang web, dữ liệu không bị mất
+
+renderData(listStudent);
